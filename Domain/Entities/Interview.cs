755732c8d67d8e9e0e4cecfb7d199e4164a9e6c5
ErgashevAzamat <azamat@thinkland.uz>
2022-12-18 00:44:42 +0500

    interview category added

--- Domain/Entities/Interview.cs
+++ Domain/Entities/Interview.cs
@@ -6,6 +6,9 @@ public class Interview : AuditableEntity
     public int UserId { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
+    public int CategoryId { get; set; }
     public List<InterviewTime> Times { get; set; }
     public List<InterviewLevel> Levels { get; set; }
+
+    public InterviewCategory Category { get; set; }
 }
