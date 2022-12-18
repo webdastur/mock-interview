00b165aa4fb1357b5f135c7e148aad8cb856dbf1
ErgashevAzamat <azamat@thinkland.uz>
2022-12-18 08:25:24 +0500

    error handling model added

---
+++ Application/Common/Model/ExceptionModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Application.Common.Model;
+
+public class ExceptionModel
+{
+    public ExceptionModel(object message, int statusCode = 500)
+    {
+        StatusCode = statusCode;
+        Message = message;
+    }
+
+    public int StatusCode { get; set; }
+    public object Message { get; set; }
+}
+
---
+++ Application/Common/Model/ResponseModel.cs
@@ -0,0 +1,45 @@
+﻿namespace Application.Common.Model;
+public class ResponseModel
+{
+    public ResponseModel()
+    {
+        StatusCode = ResponseStatusCode.Ok;
+    }
+
+    public ResponseModel(object data)
+        : this()
+    {
+        Data = data;
+    }
+
+    public ResponseModel(string message)
+        : this()
+    {
+        Message = message;
+    }
+
+    public ResponseModel(ResponseStatusCode statusCode, string message)
+        : this(message)
+    {
+        StatusCode = statusCode;
+    }
+
+    public ResponseModel(object data, ResponseStatusCode statusCode = ResponseStatusCode.Ok, string message = "")
+        : this(statusCode, message)
+    {
+        Data = data;
+    }
+
+    public object Data { get; set; }
+    public ResponseStatusCode StatusCode { get; set; }
+    public string Message { get; set; }
+
+}
+public enum ResponseStatusCode
+{
+    Ok = 200,
+    BadRequest = 400,
+    Unauthorized = 401,
+    NotFound = 404,
+    InternalServerError = 500
+}
